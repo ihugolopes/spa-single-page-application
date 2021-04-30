a1db7637e74aa00790c9147d6c23625e032f9374
Hugo Lopes <contatohugolopes@gmail.com>
2021-04-30 00:25:57 -0300

    Update MoviesService.js

--- src/api/MoviesService.js
+++ src/api/MoviesService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_KEY = 'b2fa59d81ba2421fc682478ab92d6e26';
+const API_KEY = 'token here';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 
 const withBaseUrl = path => `${BASE_URL}${path}?api_key=${API_KEY}`
